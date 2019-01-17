ca560dedd1592953a9bfe6ebfa5462eda478c64b
Aer <8425284@qq.com>
2019-01-17 22:22:33 +0800

    Update UrlModel.java

--- app/src/main/java/libsearcher/mepride/android/librarysearcher/model/UrlModel.java
+++ app/src/main/java/libsearcher/mepride/android/librarysearcher/model/UrlModel.java
@@ -3,6 +3,6 @@ package libsearcher.mepride.android.librarysearcher.model;
 public class UrlModel {
 
     public static String DoubanApi = "https://api.douban.com/v2/book/isbn/";
-    public static String ChaoXing = "http://211.86.140.145:8080/";
-    public static String Hot = "http://211.86.140.145:8080/sms/opac/news/showNewsList.action?type=3&xc=5&pageSize=10";
+    public static String ChaoXing = "http://url:8080/";
+    public static String Hot = "http://url:8080/sms/opac/news/showNewsList.action?type=3&xc=5&pageSize=10";
 }
